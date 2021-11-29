7e04a580c5c5191bab598a067e3b8d37581fc84c
Abhinav Gupta <Abhinav.Gupta@dri.edu>
2021-11-29 13:09:00 -0800

    no significant changes

--- FARIMA/FarimaParEst.py
+++ FARIMA/FarimaParEst.py
@@ -12,24 +12,24 @@ from scipy.optimize.nonlin import Jacobian
 import  statsmodels.api as sm
 import FarimaModule
 from  statsmodels.tsa.arima.model import ARIMA
+import plotFARIMA
 
 np.random.seed(10)
 
 # FARIMA model
 p = 3                               # autoregressive order
 q = 3                               # moving-average order
-d = 0.25
-arparams = np.array([0.25,0.10,0.20])    # autoregressive coefficients
-maparams = np.array([0.50,0.60,0.30])        # moving-average coefficients
+d = 0.35
+arparams = np.array([0.25,0.12,0.1])    # autoregressive coefficients
+maparams = np.array([0.90,0.85,0.20])        # moving-average coefficients
 ar = np.r_[1, -arparams]
 ma = np.r_[1, maparams]
-sigma = 1.0                         # varaince of white noise
-N = 3650                            # number of time-steps at which data will be generated
+sigma = 4                         # varaince of white noise
+N = 1000                            # number of time-steps at which data will be generated
 x = np.arange(1,N+1,1)
 
 data = sm.tsa.arma_generate_sample(ar,ma,N,sigma)
 plt.plot(data)
-plt.show()
 
 # apply inverse of the differencing operator
 if (d != 0):
@@ -94,12 +94,9 @@ while ((conv_test == False) or (count<100)):
 
 # iterative d method with subsequent local minimization
 one_sided = True
-def funcder(x):
-    fder = FarimaModule.derLw(p,q,x,f,one_sided,pxx_den)
-    fder = np.array(fder)
-    return abs(fder).tolist()
+
 pt = 100
-dvals = np.arange(0,0.5,0.01)
+dvals = np.arange(0,0.51,0.01)
 AIC_vals = []
 for d in dvals:
     if (d != 0):
@@ -108,7 +105,11 @@ for d in dvals:
         y = data
 
     model = ARIMA(y,order = (p,0,q))
-    model_fit = model.fit()
+    model.initialize_approximate_diffuse
+    model_fit = model.fit(method_kwargs={'maxiter': 1000})
+    print('**************************************************************')
+    print(model_fit.mle_retvals)
+    print('**************************************************************')
     AIC_vals.append([d,model_fit.aic])
 
 # find the optimal d (= dopt)value corresponding to minimum AIC and estimate parameters corresponding to the optimal d value  
@@ -117,20 +118,28 @@ minind = np.nonzero(AIC_vals[:,1] == AIC_vals[:,1].min())
 dopt = AIC_vals[minind[0],0]
 y = FarimaModule.diffOp(data,dopt,pt)
 model = ARIMA(y,order = (p,0,q))
-model_fit = model.fit()
+model.initialize_approximate_diffuse()
+model_fit = model.fit(method_kwargs={'maxiter': 1000})
+print('**************************************************************')
+print(model_fit.mle_retvals)
+print('**************************************************************')
 
 # extract parameters
 arparams_est = model_fit.arparams
 maparams_est = model_fit.maparams
-sigma_est = model_fit.params[-1]
+sigma_est = (model_fit.params[-1])**0.5
 params = model_fit.params
 theta = np.r_[arparams_est,maparams_est,dopt[0],sigma_est]
+
+plotFARIMA.plotResidual(model_fit.resid)
 
 # extract confidence intervals
 conf = model_fit.conf_int(0.05)
 print(conf)
 
-# Compute Hessian matrix of -2*loglikeli (Based on Whittle's approximate likelihood) and estimate the 95% confidence interval
+theta, conf_hessian1, conf_module, conf1 = FarimaModule.ParEstIterd(data,p,q,one_sided,pt,f,pxx_den)
+
+# Compute Hessian matrix of -loglikeli (Based on Whittle's approximate likelihood) and estimate the 95% confidence interval
 one_sided = True
 hess = FarimaModule.hessLw(p,q,theta,f,one_sided,pxx_den)
 Sigma2 = np.linalg.inv(hess)
